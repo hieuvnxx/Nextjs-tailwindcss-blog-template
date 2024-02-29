d6cf8f1f1f0c4ae4790bd0829c5cf6d1269a37b6
root <root@hieuvn.longvan.net>
2024-02-29 15:18:33 +0700

    sitemap config

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,21 +1,21 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    title: 'Hiusvnlabs Blog Blog With Tailwind CSS and Contentlayer',
+    author: 'Hiusvn',
+    headerTitle: 'Hiusvnlabs Blog',
+    description: 'Hiusvnlabs Blog',//'A blog created with Next.js, Tailwind.css and contentlayer.',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://hieuvnxx.com', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
-    github: 'https://github.com/codebucks27',
-    twitter: 'https://twitter.com/code_bucks',
+    email: 'hieuvnxx@gmail.com', 
+    github: 'https://github.com/hieuvnxx',
+    twitter: '#',
     facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
-    linkedin: 'https://www.linkedin.com/in/codebucks/',
-    dribbble: 'https://www.dribbble.com',
+    youtube: 'https://youtube.com',
+    linkedin: '#',
+    dribbble: '#',
     locale: 'en-US',
   }
   
